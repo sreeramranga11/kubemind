306f285aa1c713396717ec32cd5b9652513cfe5d
“Sreeram <sreeramgoudranga@gmail.com>
2025-06-16 11:57:59 -0700

    fixed trim error. now console and render error

--- kubemind-new/src/components/Cube3D.tsx
+++ kubemind-new/src/components/Cube3D.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import * as THREE from 'expo-three';
+import * as THREE from 'three';
+import { View, StyleSheet } from 'react-native';
 
 interface Cube3DProps {
   onFaceSelect: (face: string) => void;
@@ -21,7 +22,8 @@ const CubeFace: React.FC<{
   color: string;
   onClick: () => void;
 }> = ({ position, rotation, color, onClick }) => {
-  const meshRef = React.useRef<THREE.Mesh>(null);
+  const meshRef = useRef<THREE.Mesh>(null);
+
   return (
     <mesh
       ref={meshRef}
@@ -35,72 +37,83 @@ const CubeFace: React.FC<{
   );
 };
 
-const CubeGroup: React.FC<Cube3DProps> = ({ onFaceSelect, cubeState }) => {
-  const [rotation, setRotation] = React.useState<[number, number, number]>([0, 0, 0]);
-  const groupRef = React.useRef<THREE.Group>(null);
-  // Animate rotation if you want, or remove if not needed
-  // React.useEffect(() => { ... });
-  // Or useFrame for animation
-  // import { useFrame } from '@react-three/fiber';
-  // useFrame(() => { ... });
+const Cube3D: React.FC<Cube3DProps> = ({ onFaceSelect, cubeState }) => {
+  const [rotation, setRotation] = useState([0, 0, 0]);
+  const groupRef = useRef<THREE.Group>(null);
+
+  useFrame(() => {
+    if (groupRef.current) {
+      groupRef.current.rotation.x = rotation[0];
+      groupRef.current.rotation.y = rotation[1];
+      groupRef.current.rotation.z = rotation[2];
+    }
+  });
+
+  const handleFaceClick = (face: string) => {
+    onFaceSelect(face);
+  };
 
   return (
-    <group ref={groupRef} rotation={rotation}>
-      {/* Front face */}
-      <CubeFace
-        position={[0, 0, 0.5]}
-        rotation={[0, 0, 0]}
-        color={cubeState.front[1][1]}
-        onClick={() => onFaceSelect('front')}
-      />
-      {/* Back face */}
-      <CubeFace
-        position={[0, 0, -0.5]}
-        rotation={[0, Math.PI, 0]}
-        color={cubeState.back[1][1]}
-        onClick={() => onFaceSelect('back')}
-      />
-      {/* Top face */}
-      <CubeFace
-        position={[0, 0.5, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-        color={cubeState.top[1][1]}
-        onClick={() => onFaceSelect('top')}
-      />
-      {/* Bottom face */}
-      <CubeFace
-        position={[0, -0.5, 0]}
-        rotation={[Math.PI / 2, 0, 0]}
-        color={cubeState.bottom[1][1]}
-        onClick={() => onFaceSelect('bottom')}
-      />
-      {/* Left face */}
-      <CubeFace
-        position={[-0.5, 0, 0]}
-        rotation={[0, -Math.PI / 2, 0]}
-        color={cubeState.left[1][1]}
-        onClick={() => onFaceSelect('left')}
-      />
-      {/* Right face */}
-      <CubeFace
-        position={[0.5, 0, 0]}
-        rotation={[0, Math.PI / 2, 0]}
-        color={cubeState.right[1][1]}
-        onClick={() => onFaceSelect('right')}
-      />
-    </group>
+    <View style={styles.container}>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+        <group ref={groupRef}>
+          {/* Front face */}
+          <CubeFace
+            position={[0, 0, 0.5]}
+            rotation={[0, 0, 0]}
+            color={cubeState.front[1][1]}
+            onClick={() => handleFaceClick('front')}
+          />
+          {/* Back face */}
+          <CubeFace
+            position={[0, 0, -0.5]}
+            rotation={[0, Math.PI, 0]}
+            color={cubeState.back[1][1]}
+            onClick={() => handleFaceClick('back')}
+          />
+          {/* Top face */}
+          <CubeFace
+            position={[0, 0.5, 0]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            color={cubeState.top[1][1]}
+            onClick={() => handleFaceClick('top')}
+          />
+          {/* Bottom face */}
+          <CubeFace
+            position={[0, -0.5, 0]}
+            rotation={[Math.PI / 2, 0, 0]}
+            color={cubeState.bottom[1][1]}
+            onClick={() => handleFaceClick('bottom')}
+          />
+          {/* Left face */}
+          <CubeFace
+            position={[-0.5, 0, 0]}
+            rotation={[0, -Math.PI / 2, 0]}
+            color={cubeState.left[1][1]}
+            onClick={() => handleFaceClick('left')}
+          />
+          {/* Right face */}
+          <CubeFace
+            position={[0.5, 0, 0]}
+            rotation={[0, Math.PI / 2, 0]}
+            color={cubeState.right[1][1]}
+            onClick={() => handleFaceClick('right')}
+          />
+        </group>
+        <OrbitControls enablePan={false} />
+      </Canvas>
+    </View>
   );
 };
 
-const Cube3D: React.FC<Cube3DProps> = (props) => {
-  return (
-    <Canvas style={{ flex: 1 }}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <CubeGroup {...props} />
-      <OrbitControls enablePan={false} />
-    </Canvas>
-  );
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
 
-export default Cube3D; 
+export default Cube3D;
